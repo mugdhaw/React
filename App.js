f87540779b1991bcce32e1a16359001a680dfa3d
mugdha <mugdha.wadhokar@gmail.com>
2019-08-20 15:44:00 +0530

    List object remove using button
    React state and function calling nd binding

--- App.js
+++ App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
-const listArray = [ { title: 'React', url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3, points: 9, objectID: 0, }, { title: 'Redux', url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2, points: 5, objectID: 1, }, ];
+const listArray = [ { title: 'React', url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3, points: 1, objectID: 0, }, { title: 'Redux', url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2, points: 5, objectID: 1, }, ];
 
 
 class App extends Component {
-  render () {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+           list: listArray,
+        };
+        // this is the class instance , in order to define function as class method bind it in constructor
+        this.onDismiss = this.onDismiss.bind(this);
+    }
+
+    // This is EC6 definatio of function
+    onDismiss(id) {
+        console.log('onDismiss', id);
+        function isNotId(item) {
+            return item.objectID !== id;
+        }
+        const updatedList = this.state.list.filter(isNotId);
+        this.setState({ list: updatedList });
+    }
+
+    render () {
       var helloWorld = 'Test 1234';
-   return (
-       <div className="App">
-           {listArray.map(function(item){
-              return (
-                  <div key={item.objectID}>
-                      <span> <a href={item.url}>{item.title}</a> </span>
-                      <span>{item.points}</span>
-                  </div>
-              );
-           })}
-         <h2> {helloWorld} </h2>
-       </div>
-   );
-  }
+      // Object initialization in Ec5
+      const name = 'Mugdha';
+      const user = {
+          name: name,
+      }
+      return (
+           <div className="App">
+               {this.state.list.map(item => {
+                  return (
+                      <div key={item.objectID}>
+                          <span> <a href={item.url}>{item.title}</a> </span>
+                          <span>{item.points}</span>
+                          <span>{item.author}</span>
+                          <span>{item.num_comments}</span>
+                          <span>
+                              <button type="button" onClick={()=> this.onDismiss(item.objectID)}> Dismiss </button>
+                          </span>
+                      </div>
+                  );
+               })}
+             <h2> {helloWorld} </h2>
+           </div>
+      );
+    }
 }
 
 export default App;
