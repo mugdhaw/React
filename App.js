b0db061346a28d975f0239aa3becdd59113c6cde
mugdha <mugdha.wadhokar@gmail.com>
2019-08-21 15:36:17 +0530

    Search functionality on grid
    Using state and filter function of javascript

--- App.js
+++ App.js
@@ -5,15 +5,27 @@ import './App.css';
 const listArray = [ { title: 'React', url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3, points: 1, objectID: 0, }, { title: 'Redux', url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2, points: 5, objectID: 1, }, ];
 
 
+function getSearchValue(searchTerm) {
+    return function (item) {
+        // ES5 function
+        // return item.title.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
+
+        // ES6 function
+        return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+    }
+}
+
 class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
            list: listArray,
+            searchTerm: '',
         };
         // this is the class instance , in order to define function as class method bind it in constructor
         this.onDismiss = this.onDismiss.bind(this);
+        this.onSearchField = this.onSearchField.bind(this);
     }
 
     // This is EC6 definatio of function
@@ -26,6 +38,9 @@ class App extends Component {
         this.setState({ list: updatedList });
     }
 
+    onSearchField(event) {
+        this.setState({searchTerm: event.target.value });
+    }
     render () {
       var helloWorld = 'Test 1234';
       // Object initialization in Ec5
@@ -35,7 +50,10 @@ class App extends Component {
       }
       return (
            <div className="App">
-               {this.state.list.map(item => {
+               <form>
+                   <input type="text" onChange={this.onSearchField}/>
+               </form>
+               {this.state.list.filter(getSearchValue(this.state.searchTerm)).map(item => {
                   return (
                       <div key={item.objectID}>
                           <span> <a href={item.url}>{item.title}</a> </span>
@@ -43,7 +61,7 @@ class App extends Component {
                           <span>{item.author}</span>
                           <span>{item.num_comments}</span>
                           <span>
-                              <button type="button" onClick={()=> this.onDismiss(item.objectID)}> Dismiss </button>
+                              <button type="button" onClick={() => this.onDismiss(item.objectID)}> Dismiss </button>
                           </span>
                       </div>
                   );
